19c80553cd0643af103ca28f32081e7900d7bac4
Dale O Shea <daleoshea@gmail.com>
2017-10-13 19:36:57 +0100

    Final commit

--- calculator.js
+++ calculator.js
@@ -1,79 +1,58 @@
+//Methods to simply update the calculator display 
+             
+function btnThingy(){ 
+   document.calculator.result.value +="±";}
 
+function btnLeftBrac(){
+   document.calculator.result.value +="(";}
+      
+function btnRightBrac(){
+   document.calculator.result.value +=")";}
              
-             function btnThingy(){
-                 document.calculator.result.value +="±";
-             }
+function btnMinus(){
+   document.calculator.result.value +="-";}
+
+function btn7(){
+   document.calculator.result.value +="7";}
              
-             function btnLeftBrac(){
-                 document.calculator.result.value +="(";
-             }
-             function btnRightBrac(){
-                 document.calculator.result.value +=")";
-             }
+function btn8(){
+   document.calculator.result.value +="8";}
+
+function btn9(){
+   document.calculator.result.value +="9";}
+
+function btn4(){
+   document.calculator.result.value +="4";}
              
-             function btnMultiply(){
-                document.calculator.result.value +="x";
-             }
+function btn5(){
+   document.calculator.result.value +="5";}
              
-             function btnDivide(){
-                document.calculator.result.value +="÷";
-             }
+function btn6(){
+   document.calculator.result.value +="6";}
              
-             function btnMinus(){
-                 document.calculator.result.value +="-";
-             }
-             function btn7(){
-                 document.calculator.result.value +="7";
-                }
+function btn1(){
+   document.calculator.result.value +="1";}
              
-             function btn8(){
-                 document.calculator.result.value +="8";
-                }
+function btn2(){
+   document.calculator.result.value +="2";}
              
-             function btn9(){
-                 document.calculator.result.value +="9";
-                }
+function btn3(){
+    document.calculator.result.value +="3";}
              
-             function btn4(){
-                 document.calculator.result.value +="4";
-                }
+function btnPlus(){
+ document.calculator.result.value +="+";}
              
-             function btn5(){
-                 document.calculator.result.value +="5";
-                }
+function btn0(){
+   document.calculator.result.value +="0";}
+ 
+// method to evalute the given values with the cal display 
+// returns the result to the display
+function equal(){
+   var res = eval(document.calculator.result.value);
+   document.calculator.result.value = res;
+}
              
-             function btn6(){
-                 document.calculator.result.value +="6";
-                }
-             
-             function btn1(){
-                 document.calculator.result.value +="1";
-                }
-             
-             function btn2(){
-                 document.calculator.result.value +="2";
-                }
-             
-             function btn3(){
-                 document.calculator.result.value +="3";
-                }
-             
-             function btnPlus(){
-                 document.calculator.result.value +="+";
-             }
-             
-             function btn0(){
-                 document.calculator.result.value +="0";
-                }
-             
-             function equal(){
-                 
-                 var res = eval(document.calculator.result.value);
-                 document.calculator.result.value = res;
-             }
-             
-             function clr(){  
-                 
-                 document.calculator.result.value = " ";
-             }
+function clr(){       
+   document.calculator.result.value = " ";
+}
     
